c51d309ca48394f0ee1387b4dc000f6ea27b2c96
Ciremun <Ciremun@yandex.ru>
2022-02-14 18:54:41 +0300

    dev env

---
+++ app.py
@@ -0,0 +1,9 @@
+from src.common import *
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.jinja", **globals())

546aece8ee05d6d04ac88efeaa2ce1ca2a6ddf9f
Ciremun <Ciremun@yandex.ru>
2022-02-15 19:41:30 +0300

    fallback to main.css if main.min.css build failed

--- build.py
+++ build.py
@@ -10,15 +10,16 @@ main_min_css_path = 'static/main.min.css'
 main_css_path = 'static/main.css'
 main_min_css = minify_css(main_css_path)
 
-with open(main_min_css_path, "w") as f:
-    f.write(main_min_css)
+if main_min_css:
+    with open(main_min_css_path, "w") as f:
+        f.write(main_min_css)
 
 # Evaluate Template
 from src.template_vars import *
 
 from jinja2 import Environment, FileSystemLoader
 
-main_css = main_min_css_path
+main_css = main_min_css_path if main_min_css else main_css_path
 
 env = Environment(loader=FileSystemLoader("."), trim_blocks=True, lstrip_blocks=True)
 template = env.get_template("templates/index.jinja")
